eec06abb05f168c3c36e22b69b1a774def3dae03
yakisugita <yakisugita@gmail.com>
2024-01-14 21:21:54 +0900

    共有メモリから値取得して表示

--- VoiceNaviPlus/Form1.Designer.cs
+++ VoiceNaviPlus/Form1.Designer.cs
@@ -29,12 +29,42 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(229, 49);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- VoiceNaviPlus/Form1.cs
+++ VoiceNaviPlus/Form1.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace VoiceNaviPlus
 {
@@ -15,6 +18,44 @@ namespace VoiceNaviPlus
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string last_voice = "";
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                // 共有メモリ開く
+                MemoryMappedFile share_mem = MemoryMappedFile.OpenExisting("YAKIJAKE_MEM");
+                MemoryMappedViewAccessor accessor = share_mem.CreateViewAccessor();
+
+                // 共有メモリから読み出し
+                char[] data3 = new char[64];
+                int readsize = accessor.ReadArray<char>(0, data3, 0, 64);
+                string voice = new string(data3);
+
+                // 変わった時だけ処理する
+                if (voice != last_voice)
+                {
+                    label1.Text = voice;
+                    Console.WriteLine(voice.ToString());
+                }
+                last_voice = voice;
+
+                // Dispose resource
+                accessor.Dispose();
+                share_mem.Dispose();
+            }
+            catch (Exception error)
+            {
+                Debug.Write(error);
+            }
         }
     }
 }
