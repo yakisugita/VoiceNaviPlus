843fee1f9adf209281d0d3c7ee83c3513ac1b608
yakisugita <yakisugita@gmail.com>
2024-01-20 19:56:12 +0900

    v0.1,メインウィンドウのタイトル変更

--- VoiceNaviPlus/Form1.Designer.cs
+++ VoiceNaviPlus/Form1.Designer.cs
@@ -241,7 +241,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "VoiceNaviPlus / こえナビ+ v0.1";
             this.TopMost = true;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.statusStrip1.ResumeLayout(false);
